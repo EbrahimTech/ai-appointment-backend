67da137088fb0c00fa79f9bf389b756bbcbb95cb
Ebrahim Khaled <ebrahimtech1@gmail.com>
2025-12-20 19:17:05 +0300

    Fixing

--- backend/asgi.py
+++ backend/asgi.py
@@ -11,11 +11,15 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.conf import settings
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 django_asgi_app = get_asgi_application()
+if settings.DEBUG:
+    django_asgi_app = ASGIStaticFilesHandler(django_asgi_app)
 
 from apps.conversations.routing import websocket_urlpatterns
 
--- backend/settings.py
+++ backend/settings.py
@@ -175,7 +175,7 @@ USE_TZ = True
 
 # Static files
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Outbound integrations (WhatsApp, Google, LLMs)
